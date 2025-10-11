595b4c4401540eaa5c0c64b37936365c469a40ea
Takiullin Timur <timurtakiullin@icloud.com>
2025-10-11 17:24:10 +0300

    Update asgi.py

--- BrandBook/Back/myproject/asgi.py
+++ BrandBook/Back/myproject/asgi.py
@@ -17,20 +17,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
-# import os
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-# from myproject import urls  # Замените 'your_app' на имя вашего приложения
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             urls.websocket_urlpatterns
-#         )
-#     ),
-# })
